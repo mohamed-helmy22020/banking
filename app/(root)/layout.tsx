d807deb0dfc77494bac33b8a8f595fee36f941ad
Mohamed Helmy <petxtv@gmail.com>
2024-08-24 19:56:01 +0300

    add force dynamic

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import MobileNav from "@/components/MobileNav";
 import SideBar from "@/components/SideBar";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
